404eeb1a4f221d561a8c76cd904472f75ff5a8ae
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-08-11 22:02:37 -0400

    Initial Commit

---
+++ ViewsandModifiers1/ViewsandModifiers1App.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsandModifiers1App.swift
+//  ViewsandModifiers1
+//
+//  Created by Jason Cox on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewsandModifiers1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
