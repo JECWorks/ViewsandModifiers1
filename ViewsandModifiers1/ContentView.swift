9602bee84ee8af98ac3ac9c751a7f57adab85d8d
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-08-20 21:59:38 -0400

    Custom containers

--- ViewsandModifiers1/ContentView.swift
+++ ViewsandModifiers1/ContentView.swift
@@ -20,6 +20,44 @@ struct CapsuleText: View {
     
 }
 
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundStyle(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(.rect(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+            ZStack(alignment: .bottomTrailing) {
+                content
+                Text(text)
+                    .font(.caption)
+                    .foregroundStyle(.white)
+                    .padding(5)
+                    .background(.green)
+            }
+        }
+}
+
 struct ContentView: View {
     @State private var useRedText = true
     var motto1: some View {
@@ -40,53 +78,9 @@ struct ContentView: View {
     }
     
     var body: some View {
-        VStack {
-            Text("Hello, world!")
-                .padding()
-                .background(.red)
-                .padding()
-                .background(.blue)
-                .padding()
-                .background(.green)
-                .padding()
-                .background(.yellow)
-            motto1
-                .foregroundStyle(.green)
-            motto2
-                .foregroundStyle(.teal)
-            spells
-                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-            
-            
-        }
-        VStack {
-            Button("Hello World") {
-                // Conditional modifiers Whatyou'rechecking ? True : False
-                        // flip the Boolean between true and false
-                        useRedText.toggle()
-                    }
-            .foregroundStyle(useRedText ? .red : .blue)
-            
-            if useRedText {
-                    Button("Hello World") {
-                        useRedText.toggle()
-                    }
-                    .foregroundStyle(.red)
-                } else {
-                    Button("Hello World") {
-                        useRedText.toggle()
-                    }
-                    .foregroundStyle(.blue)
-                }
-        }
-        VStack {
-            Text("Gryffindor")
-                .blur(radius: 5.0)
-            Text("Hufflepuff")
-            Text("Ravenclaw")
-            Text("Slytherin")
-        }
-        .font(.title)
+        Color.teal
+            .frame(width: 300, height: 200)
+            .watermarked(with: "Hacking with Swift")
         
         VStack(spacing: 10) {
                 Text("First")
@@ -98,7 +92,7 @@ struct ContentView: View {
 
                 CapsuleText(text: "Second")
                 CapsuleText(text: "Third")
-                    .foregroundStyle(.yellow)
+                    .modifier(Title())
                 }
     }
 }
