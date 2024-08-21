64a28717dc74b22d8412eb56d1952656e0556a10
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-08-20 22:13:10 -0400

    More Custom Containers

--- ViewsandModifiers1/ContentView.swift
+++ ViewsandModifiers1/ContentView.swift
@@ -7,93 +7,42 @@
 
 import SwiftUI
 
-struct CapsuleText: View {
-    var text: String
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    @ViewBuilder let content: (Int, Int) -> Content
+    
     var body: some View {
-        Text(text)
-            .font(.largeTitle)
-            .padding()
-//            .foregroundStyle(.white)
-            .background(.blue)
-            .clipShape(.capsule)
-    }
-    
-}
-
-struct Title: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .foregroundStyle(.white)
-            .padding()
-            .background(.blue)
-            .clipShape(.rect(cornerRadius: 10))
-    }
-}
-
-extension View {
-    func titleStyle() -> some View {
-        modifier(Title())
-    }
-}
-
-extension View {
-    func watermarked(with text: String) -> some View {
-        modifier(Watermark(text: text))
-    }
-}
-
-struct Watermark: ViewModifier {
-    var text: String
-    
-    func body(content: Content) -> some View {
-            ZStack(alignment: .bottomTrailing) {
-                content
-                Text(text)
-                    .font(.caption)
-                    .foregroundStyle(.white)
-                    .padding(5)
-                    .background(.green)
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<columns, id: \.self) { column in
+                        content(row, column)
+                    }
+                }
             }
         }
+    }
 }
 
 struct ContentView: View {
-    @State private var useRedText = true
-    var motto1: some View {
-        Text("Draco dormiens")
-    }
-    let motto2 = Text("nunquam titillandus")
-    
-    var spells: some View {
-        Group {
-            Text("Lumos")
-            Text("Obliviate")
-        }
-    }
-    
-    @ViewBuilder var spells2: some View {
-        Text("Oculus reparo")
-        Text("Expelliarmus")
-    }
-    
     var body: some View {
-        Color.teal
-            .frame(width: 300, height: 200)
-            .watermarked(with: "Hacking with Swift")
-        
-        VStack(spacing: 10) {
-                Text("First")
-                    .font(.largeTitle)
-                    .padding()
-                    .foregroundStyle(.white)
-                    .background(.blue)
-                    .clipShape(.capsule)
-
-                CapsuleText(text: "Second")
-                CapsuleText(text: "Third")
-                    .modifier(Title())
-                }
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            GridStack(rows: 4, columns: 4) { row, col in
+                Text("R\(row) C\(col)")
+            }
+            
+            .padding()
+            
+            GridStack(rows: 4, columns: 4) { row, col in
+                Image(systemName: "\(row * 4 + col).circle")
+                Text("R\(row) C\(col)")
+                
+            }
+        }
     }
 }
 
